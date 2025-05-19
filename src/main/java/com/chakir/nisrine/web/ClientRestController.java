e9f030299280b725b6948808bdc76b5b571e34cc
Nisrine-C <nisrinechakir237@gmail.com>
2025-05-19 11:34:15 +0100

    Added some pre authorities

--- src/main/java/com/chakir/nisrine/web/ClientRestController.java
+++ src/main/java/com/chakir/nisrine/web/ClientRestController.java
@@ -5,6 +5,7 @@ import com.chakir.nisrine.exceptions.ClientNotFoundException;
 import com.chakir.nisrine.services.BankCreditService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,32 +18,38 @@ public class ClientRestController {
     private final BankCreditService bankCreditService;
 
     @GetMapping("clients")
+    @PreAuthorize("hasAuthority('SCOPER EMPLOYE_ROLE')")
     public List<ClientDTO> listClients() {
         return bankCreditService.listClients();
     }
 
     @GetMapping("/clients/search")
+    @PreAuthorize("hasAuthority('SCOPER EMPLOYE_ROLE')")
     public List<ClientDTO> searchClients(@RequestParam(name = "keyword", defaultValue = "") String keyword) {
         return bankCreditService.searchClients("%" + keyword + "%");
     }
 
     @GetMapping("/clients/{id}")
+    @PreAuthorize("hasAuthority('SCOPER CLIENT_ROLE')")
     public ClientDTO getClient(@PathVariable Long id) throws ClientNotFoundException {
         return bankCreditService.getClient(id);
     }
 
     @PostMapping("/clients")
+    @PreAuthorize("hasAuthority('SCOPER ADMIN_ROLE')")
     public ClientDTO saveClient(@RequestBody ClientDTO clientDTO) {
         return bankCreditService.saveClient(clientDTO);
     }
 
     @PutMapping("/clients/{id}")
+    @PreAuthorize("hasAuthority('SCOPER ADMIN_ROLE')")
     public ClientDTO updateClient(@PathVariable Long id, @RequestBody ClientDTO clientDTO) {
         clientDTO.setId(id);
         return bankCreditService.updateClient(clientDTO);
     }
 
     @DeleteMapping("/clients/{id}")
+    @PreAuthorize("hasAuthority('SCOPER ADMIN_ROLE')")
     public void deleteClient(@PathVariable Long id) {
         bankCreditService.deleteClient(id);
     }
--- src/main/java/com/chakir/nisrine/web/CreditRestController.java
+++ src/main/java/com/chakir/nisrine/web/CreditRestController.java
@@ -4,8 +4,8 @@ import com.chakir.nisrine.dtos.*;
 import com.chakir.nisrine.exceptions.ClientNotFoundException;
 import com.chakir.nisrine.exceptions.CreditNotFoundException;
 import com.chakir.nisrine.services.BankCreditService;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,59 +14,70 @@ import java.util.List;
 @Slf4j
 @RequestMapping("/credits")
 @CrossOrigin("*")
-public class BankCreditRestController {
+public class CreditRestController {
     private final BankCreditService bankCreditService;
 
-    public BankCreditRestController(BankCreditService bankCreditService) {
+    public CreditRestController(BankCreditService bankCreditService) {
         this.bankCreditService = bankCreditService;
     }
 
     @GetMapping("/{creditId}")
+    @PreAuthorize("hasAuthority('SCOPER CLIENT_ROLE')")
     public CreditDTO getCredit(@PathVariable Long creditId) throws CreditNotFoundException {
         return bankCreditService.getCredit(creditId);
     }
 
     @GetMapping
+    @PreAuthorize("hasAuthority('SCOPER EMPLOYE_ROLE')")
     public List<CreditDTO> listCredits() {
         return bankCreditService.listCredits();
     }
 
+    @PreAuthorize("hasAuthority('SCOPER CLIENT_ROLE')")
     @GetMapping("/client/{clientId}")
     public List<CreditDTO> getClientCredits(@PathVariable Long clientId) throws ClientNotFoundException {
         return bankCreditService.listCreditsByClient(clientId);
     }
 
+    @PreAuthorize("hasAuthority('SCOPER CLIENT_ROLE')")
     @GetMapping("/{creditId}/repayments")
     public List<RemboursementDTO> getCreditRepayments(@PathVariable Long creditId) throws CreditNotFoundException {
         return bankCreditService.getRemboursementsByCredit(creditId);
     }
 
     @PostMapping("/personal")
+    @PreAuthorize("hasAuthority('SCOPER CLIENT_ROLE')")
     public CreditPersonnelDTO createPersonalCredit(@RequestBody CreditPersonnelDTO creditDTO) throws ClientNotFoundException {
         return bankCreditService.saveCreditPersonnel(creditDTO);
     }
 
     @PostMapping("/mortgage")
+    @PreAuthorize("hasAuthority('SCOPER CLIENT_ROLE')")
     public CreditImmobilierDTO createMortgageCredit(@RequestBody CreditImmobilierDTO creditDTO) throws ClientNotFoundException {
         return bankCreditService.saveCreditImmobilier(creditDTO);
     }
 
     @PostMapping("/business")
+    @PreAuthorize("hasAuthority('SCOPER EMPLOYE_ROLE')")
     public CreditProfessionelDTO createBusinessCredit(@RequestBody CreditProfessionelDTO creditDTO) throws ClientNotFoundException {
         return bankCreditService.saveCreditProfessionel(creditDTO);
     }
 
     @PostMapping("/{creditId}/repayments")
+    @PreAuthorize("hasAuthority('SCOPER EMPLOYE_ROLE')")
     public RemboursementDTO addRepayment(@PathVariable Long creditId, @RequestBody RemboursementDTO repaymentDTO) throws CreditNotFoundException {
         return bankCreditService.addRemboursement(creditId, repaymentDTO);
     }
 
+
     @PutMapping("/{creditId}/status")
+    @PreAuthorize("hasAuthority('SCOPER EMPLOYE_ROLE')")
     public void updateStatus(@PathVariable Long creditId, @RequestParam String status) throws CreditNotFoundException {
         bankCreditService.updateCreditStatus(creditId, status);
     }
 
     @DeleteMapping("/{creditId}")
+    @PreAuthorize("hasAuthority('SCOPER EMPLOYE_ROLE')")
     public void deleteCredit(@PathVariable Long creditId) {
         bankCreditService.deleteCredit(creditId);
     }
