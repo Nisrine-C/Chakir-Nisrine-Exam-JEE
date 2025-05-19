8bfe5523589e82c0df03116eb38162d3128bf106
Nisrine-C <nisrinechakir237@gmail.com>
2025-05-19 08:56:15 +0100

    Initial Commit

---
+++ src/main/java/com/chakir/nisrine/ChakirNisrineExamJeeApplication.java
@@ -0,0 +1,13 @@
+package com.chakir.nisrine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChakirNisrineExamJeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChakirNisrineExamJeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chakir/nisrine/ChakirNisrineExamJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chakir.nisrine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChakirNisrineExamJeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
